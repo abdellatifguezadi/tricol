0056996c4d7980c848bf3945a13405c84905d4f3
abdellatif guezadi <abdellatifguezadi@gmail.com>
2025-10-27 09:42:26 +0100

    refactor remove commented derived query methods in FournisseurRepository

--- src/main/java/com/tricol/repository/FournisseurRepository.java
+++ src/main/java/com/tricol/repository/FournisseurRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
 
-    // Derived query methods to return sorted results by 'societe'
     List<Fournisseur> findAllByOrderBySocieteAsc();
     List<Fournisseur> findAllByOrderBySocieteDesc();
 
